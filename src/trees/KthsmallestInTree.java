5706b983ef65bf7be14b999dc3eb861a4620b646
WISEYNAB <109348369+WISEYNAB@users.noreply.github.com>
2025-09-13 13:49:29 +0530

    optimal approach kth smalles element in a bst using inorder

--- src/trees/KthsmallestInTree.java
+++ src/trees/KthsmallestInTree.java
@@ -37,3 +37,48 @@ class Solution {
         traverse(root.right);
     }
 }
+
+//optimal
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int m;
+    int ans;
+
+    public int kthSmallest(TreeNode root, int k) {
+        m = k;
+        ans = 0;
+
+        traverse(root);
+        return ans;
+    }
+
+    void traverse(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        traverse(root.left);
+
+        m--;
+        if (m == 0) {
+            ans = root.val;
+            return;
+        }
+
+        traverse(root.right);
+    }
+}
