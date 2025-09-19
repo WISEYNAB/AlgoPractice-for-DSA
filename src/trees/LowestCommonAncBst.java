c2d28fe45bf218c6cab298fedff0e1c10bd30de3
WISEYNAB <109348369+WISEYNAB@users.noreply.github.com>
2025-09-19 09:50:16 +0530

    lca bst optimal

--- src/trees/LowestCommonAncBst.java
+++ src/trees/LowestCommonAncBst.java
@@ -33,3 +33,48 @@ class Solution235 {
         return left != null ? left : right;
     }
 }
+
+//optimal
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solutionopt {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return lcs(root,p,q);
+    }
+
+    TreeNode lcs(TreeNode root,TreeNode p,TreeNode q){
+        if(root == null){
+            return null;
+        }
+
+        if(root == p || root == q){
+            return root;
+        }
+
+        if(p.val > root.val && q.val > root.val){
+            return lcs(root.right,p,q);
+        }
+
+        if(p.val < root.val && q.val < root.val){
+            return lcs(root.left,p,q);
+        }
+
+        TreeNode left = lcs(root.left,p,q);
+        TreeNode right = lcs(root.right,p,q);
+
+        if(left != null && right != null){
+            return root;
+        }
+
+        return left != null ? left : right;
+    }
+}
