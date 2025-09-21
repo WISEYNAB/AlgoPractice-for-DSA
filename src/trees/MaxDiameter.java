f9b75e3421d4fcb9b33f69e3eb77954a3f870e41
WISEYNAB <109348369+WISEYNAB@users.noreply.github.com>
2025-09-21 08:12:28 +0530

    leetcode trees max diameter

---
+++ src/trees/MaxDiameter.java
@@ -0,0 +1,38 @@
+package trees;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int dia;
+    public int diameterOfBinaryTree(TreeNode root) {
+        dia = 0;
+        solve(root);
+        return dia;
+    }
+
+    int solve(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+
+        int left = solve(root.left);
+        int right = solve(root.right);
+
+        dia = Math.max(dia,left + right);
+
+        return 1 + Math.max(left,right);
+    }
+}
